edc70b17cfa9492e42668405235d77c3e2d8a004
8khzkdv2dv-jpg <8khzkdv2dv@privaterelay.appleid.com>
2026-06-17 00:02:05 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='planr-v20';
+const CACHE='planr-v21';
 const FILES=['/Planr/', '/Planr/index.html', '/Planr/manifest.json', '/Planr/icon-192.png', '/Planr/icon-512.png'];
 
 self.addEventListener('install', e=>{
